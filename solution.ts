2e804d38e66adda9c5c3833c95a2979d8a6bcc2e
hadar <hadargossels@gmail.com>
2021-01-30 19:30:33 +0200

    create class named TASK

---
+++ solution.ts
@@ -0,0 +1,7 @@
+class TASK {
+    private headLine: string; private done: string
+    constructor (headLine:string, done:string) {
+        this.headLine = headLine
+        this.done = "no"
+    } 
+}
